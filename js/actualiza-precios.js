2d86debf3942b09a628f32d0f1abc4dfc5ee706b
Seccab <gonzalo.cabrera@seccab.com>
2025-06-12 11:33:08 -0600

    Portatil 12jun 11:30am

--- js/actualiza-precios.js
+++ js/actualiza-precios.js
@@ -10,23 +10,34 @@ $(function () {
 
     var listaDistri = new Array();
 
-
+    var lista_precios_gen = new Array();
+    var lista_precios_ind = new Array();
 
 
 
     const $txtCodDistri = $('#txtCodDistri');
     const $txtNomDistri = $('#txtNomDistri');
-
-
-
-
     const $btnBuscaDis = $('#btnBuscaDis');
 
+    const $txtPreGen = $('#txtPreGen').val('0.00');
+    const $txtPreInd = $('#txtPreInd').val('0.00');
 
 
-    const $btnPreGen = $('#btnPreGen');
 
+    const $btnActPreGen = $('#btnActPreGen');
+    const $btnActPreInd = $('#btnActPreInd');
 
+    const $txtFecIniGen = $('#txtFecIniGen')
+        .val(obtieneFechaActual())
+        .change(function () {
+
+        });
+
+    const $txtFecIniInd = $('#txtFecIniInd')
+        .val(obtieneFechaActual())
+        .change(function () {
+
+        });
 
 
 
@@ -34,7 +45,7 @@ $(function () {
 
 
 
-        $tblDistri = $('#tblDistri').DataTable({
+        var $tblDistri = $('#tblDistri').DataTable({
 
             destroy: true,
             data: listaDistri,
@@ -73,10 +84,130 @@ $(function () {
 
             $txtCodDistri.val(fila.cod_dis);
             $txtNomDistri.val(fila.nom_dis);
+        });
 
+
+
+        var $tblPreGen = $('#tblPreGen').DataTable({
+            //destroy: true,
+            responsive: true,
+            data: lista_precios_gen,
+            columns: [
+
+
+                {
+                    data: 'fec_ini'
+
+                },
+                {
+                    data: 'pre_item',
+                    className: 'text-end',
+                    render: DataTable.render.number(',', '.'),
+                    searchable: false
+
+                },
+                {
+                    defaultContent: '<button class="editar btn btn-primary"><i class="bi bi-pen"></i></button> <button class="eliminar btn btn-danger"><i class="bi bi-x"></i></button>',
+                    className: 'text-center'
+
+                }
+
+            ],
+            info: false,
+            ordering: false,
+            language: lenguaje_data_table
+        });
+
+
+        $tblPreGen.on('click', 'button.editar', function () {
+
+            let fila = $tblPreGen.row($(this).parents('tr')).data();
 
 
         });
+
+        $tblPreGen.on('click', 'button.eliminar', function () {
+
+            let fila = $tblPreGen.row($(this).parents('tr')).data();
+
+
+            Swal
+                .fire({
+                    title: "Desea Eliminar el precio registrado?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: "Sí, eliminar",
+                    cancelButtonText: "Cancelar",
+                })
+                .then(resultado => {
+                    if (resultado.value) {
+
+                    }
+                });
+
+        });
+
+
+        var $tblPreInd = $('#tblPreInd').DataTable({
+            //destroy: true,
+            responsive: true,
+            data: lista_precios_ind,
+            columns: [
+
+
+                {
+                    data: 'fec_ini'
+
+                },
+                {
+                    data: 'pre_item',
+                    className: 'text-end',
+                    render: DataTable.render.number(',', '.'),
+                    searchable: false
+
+                },
+                {
+                    defaultContent: '<button class="editar btn btn-primary"><i class="bi bi-pen"></i></button> <button class="eliminar btn btn-danger"><i class="bi bi-x"></i></button>',
+                    className: 'text-center'
+
+                }
+
+            ],
+            info: false,
+            ordering: false,
+            language: lenguaje_data_table
+        });
+
+
+        $tblPreInd.on('click', 'button.editar', function () {
+
+            let fila = $tblPreInd.row($(this).parents('tr')).data();
+
+
+        });
+
+        $tblPreInd.on('click', 'button.eliminar', function () {
+
+            let fila = $tblPreInd.row($(this).parents('tr')).data();
+
+
+            Swal
+                .fire({
+                    title: "Desea Eliminar el precio registrado?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: "Sí, eliminar",
+                    cancelButtonText: "Cancelar",
+                })
+                .then(resultado => {
+                    if (resultado.value) {
+
+                    }
+                });
+
+        });
+
+
 
         $txtCodDistri
             .focus(function () {
@@ -102,20 +233,6 @@ $(function () {
                 }
             });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         $btnBuscaDis.click(function (e) {
 
             //$('#modBuscaDis').modal('show');
@@ -127,15 +244,52 @@ $(function () {
         });
 
 
+        $txtPreGen
+            .focus(function () {
+                $(this).select();
+
+            }).keydown(function (e) {
+                let code = e.keyCode || e.which;
+                if (code == 13 || code == 9) {
+
+                    let x = Number.parseFloat($(this).val());
+                    $(this).val(nf_decimal.format(x));
+
+                    $btnActPreGen.focus();
+                    e.preventDefault();
+                }
+            });
+
+        $txtPreInd
+            .focus(function () {
+                $(this).select();
+
+            }).keydown(function (e) {
+                let code = e.keyCode || e.which;
+                if (code == 13 || code == 9) {
+
+                    let x = Number.parseFloat($(this).val());
+                    $(this).val(nf_decimal.format(x));
+
+                    $btnActPreInd.focus();
+                    e.preventDefault();
+                }
+            });
 
 
 
-        $btnPreGen.click(function (e) {
+        $btnActPreGen.click(function (e) {
 
-          
+
             e.preventDefault();
         });
 
+
+        $btnActPreInd.click(function (e) {
+
+
+            e.preventDefault();
+        });
 
 
 
@@ -220,10 +374,5 @@ $(function () {
     }
 
     ini_componentes();
-
-
-
-
-
 
 });
